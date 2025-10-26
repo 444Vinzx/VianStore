1f3e8ff195705439a0548eba1942db330f0254a5
444Vinzx <arfianramadhan12@gmail.com>
2025-10-26 19:45:28 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function showQRIS(product) {
-  document.getElementById("qrisTitle").innerText = "Pembayaran " + product;
-  document.getElementById("qrisModal").style.display = "flex";
-}
-function closeQRIS() {
-  document.getElementById("qrisModal").style.display = "none";
-}
